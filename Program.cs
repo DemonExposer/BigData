95fcab16a3210e34331ef0ba8d8676b7a1055636
Kelvin Bouma <kbdmns@gmail.com>
2021-06-10 21:05:24 +0200

    v1.0

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BigData {
+	public class Program {
+		public async static Task Main(string[] args) {
+			StreamWriter file = new StreamWriter("bigData.txt", true);
+			Random random = new Random();
+			while (true)
+				await file.WriteAsync((char) random.Next(256));
+		}
+	}
+}
